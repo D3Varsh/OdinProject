0bd4766e4cda267f95644d541c322d8e9d403058
Devarsh <devarsh12092003@gmail.com>
2024-11-04 15:23:48 -0500

    add function.js

---
+++ function.js
@@ -0,0 +1,12 @@
+function favouriteAnimal(animal){
+    return animal + " is my favorite animal!"
+
+
+}
+
+console.log(favouriteAnimal('Tiger'));
+
+
+const myText =  "I am a String";
+const newString = myText.replace("String", "sausage");
+console.log(newString);
