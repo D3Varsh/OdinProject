82adfe0dede4c2664d26d4d11794bc44739c5413
Devarsh <devarsh12092003@gmail.com>
2024-11-15 19:05:12 -0500

    Add Array,js

---
+++ array.js
@@ -0,0 +1,32 @@
+// CREATING AN ARRAY 
+
+const cars  = ["Rollys","Beamer","Volvo"];
+
+// Loop to print each element of this array
+
+
+for (i = 0; i < cars.length;i++)
+{
+    console.log(cars[i]);
+}
+
+// This is the sort function it will make the array alphabetwise
+console.log(cars.sort());
+
+/* This all are the methodes for the array. 
+Array.length()     For the lenght of array
+Array.toString()    To conver to string
+Array.at()          Return index and element
+Array.join()
+Array.pop()
+Array.push()
+Array.shift()
+Array.unshift()
+Array.delete()
+Array.concat()
+Array.copyWithin()
+Array.flat()
+Array.splice()
+Array.toSpliced()
+Array.slice()
+*/
