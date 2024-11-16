7e792e30eeab930d870f538fc561e4280ec70037
Devarsh <devarsh12092003@gmail.com>
2024-11-15 19:09:41 -0500

    Add Loop.js

---
+++ loop.js
@@ -0,0 +1,28 @@
+// The While loop
+```
+while (condition){
+}
+```
+
+let i = 0;
+while (i < 3) { // shows 0, then 1, then 2
+  alert( i );
+  i++;
+}
+
+// The Do while loop
+
+let i1 = 0;
+do {
+  alert( i1 );
+  i1++;
+} while (i1 < 3);
+
+// The for loop
+
+```
+for (let i = 0; i < 3; i++) { // shows 0, then 1, then 2
+  alert(i);
+}
+
+```
